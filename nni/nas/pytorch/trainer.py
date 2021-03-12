dcac65558f5e0dd73a08ce33f66d810816735116
TimofeevAlex <timofeev.am@phystech.edu>
2021-03-12 18:56:31 +0300

    fix import

--- nni/nas/pytorch/trainer.py
+++ nni/nas/pytorch/trainer.py
@@ -5,7 +5,7 @@ import json
 import logging
 import os
 import time
-import nunpy as np
+import numpy as np
 # sys.path.append('../../../examples/nas/darts')
 from .model import CNN
 from sklearn.manifold import TSNE
